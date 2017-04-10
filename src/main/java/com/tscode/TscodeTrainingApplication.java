e2775c974bca80d31dc140016d39b248450bdf28
Lang <silentbalanceyh@126.com>
2017-04-10 11:51:48 +0800

    Init Tradeshift Training Code

---
+++ src/main/java/com/tscode/TscodeTrainingApplication.java
@@ -0,0 +1,12 @@
+package com.tscode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TscodeTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TscodeTrainingApplication.class, args);
+	}
+}
